d3774843c202286e7429e5a474188e12e560fc06
exler <kakkmar@gmail.com>
2018-08-01 12:49:22 +0200

    Change view engine, add .env and SQLite database

--- setup.sh
+++ setup.sh
@@ -8,19 +8,26 @@ if [ "$EUID" -ne 0 ]; then
 	exit 1
 fi
 
-# Ask for password to use MySQL with
-read -s -p "[?] MySQL password: " MYSQL_PASS
+# Set new password
+read -s -p "[?] New password: " NEW_PASSWORD
+echo "pi:${NEW_PASSWORD}" | sudo chpasswd
 
-# Updating system and downloading necessary packages
-echo -e "[+] Updating system and downloading packages..\n"
+# Updating system and installing packages
+echo -e "[+] Updating system and installing packages..\n"
 sudo apt update
 sudo apt full-upgrade
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASS"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASS"
-sudo apt install -y nodejs mysql-server
-sudo npm install -g nodemon
+sudo apt install -y nodejs
+sudo npm install -g pm2
+
+# Starting web interface
+echo -e "[+] Starting web interace.. Default port: 4803\n"
+pm2 start web/bin/www --name winogrono-web
+pm2 startup
 
 # Cleaning up
-echo -e "[+] Cleaning up after install.."
+echo -e "[+] Cleaning up after install..\n"
 sudo apt autoremove
+
+# Reboot
+sudo reboot
--- web/app.js
+++ web/app.js
@@ -1,17 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
+
+var httpErrors = require('http-errors');
+var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var dotenv = require('dotenv').config()
+var sqlite3 = require('sqlite3').verbose()
 
-var indexRouter = require('./routes/index');
+var routes = require('./routes');
 
 var app = express();
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,12 +28,15 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Database connection
+var db = new sqlite3.Database(process.env.DB_NAME)
+
 // Routing
-app.use('/', indexRouter);
+app.get('/', routes.index);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(httpErrors(404));
 });
 
 // Error handler
